059d26002d2311fd2b9155cfeb2e2e85000f2980
Andrea Bellandi <andrea.bellandi@desy.de>
2019-11-22 18:05:27 +0100

    Debugged FromVhdl. Bug in Layout

--- src/main/scala/hdltosvg/vhdl/FromVhdl.scala
+++ src/main/scala/hdltosvg/vhdl/FromVhdl.scala
@@ -1,16 +1,26 @@
 package hdltosvg.vhdl
+import java.io.FileOutputStream
 import hdltosvg._
+import scala.io.Source
 
 object fromVhdl {
+
+  def main(args: Array[String]): Unit = {
+    val source = Source.fromFile(args(0)).getLines.map(removeComments(_)).mkString
+    val components = getAll(source).map(getComponentInfo(_))
+    components.foreach { 
+      cmp => ComponentSVG.fromUnsorted(cmp, 
+        new FileOutputStream(cmp.componentName ++ ".svg"),
+        OutputConfig.default()) 
+    }
+  }
 
   def removeComments(rawVhdlSource : String) : String = {
     rawVhdlSource.replaceAll("--.*","")
   }
 
   def getPorts(vhdlSource : String, keyword : String) : Array[(String,String)] = {
-    val re = ("""(?i)(?s)^\s*""" + 
-              keyword + 
-              """\s+(\w*)\s+is.*?port\s*\((.*?)\);\s*end(?:\s|;)""").r
+    val re = (keyword + """\s+(\w*)\s+is.*?port\s*\((.*?)\);\s*end(?:\s|;)""").r
   
     re.findAllIn(vhdlSource).matchData.map(m => (m.group(1), m.group(2))).toArray
   }
@@ -24,17 +34,17 @@ object fromVhdl {
   def getComponentInfo(rawComponent : (String,String)) : ComponentInfo = {
     
     val (componentName, body) = rawComponent
+    val arrowStyle = ArrowStyle.StyleReentrant
     val bodyMerged = body.replaceAll("[\n\r]","")
     val elements = bodyMerged.split(";").map(_.trim).toArray
-    val inRe = """(?i)(\w*)^\s*:\s*in\s*(*.)$""".r
-    val outRe = """(?i)(\w*)^\s*:\s*out\s*(*.)$""".r
-    val inoutRe = """(?i)(\w*)^\s*:\s*inout\s*(*.)$""".r
+    val inRe = """(\w*)\s*:\s*in\s*(.*)""".r
+    val outRe = """(\w*)\s*:\s*out\s*(.*)""".r
+    val inoutRe = """(\w*)\s*:\s*inout\s*(.*)""".r
     val groupsLeft = elements.map(_ match {
-      case inRe(name, desc) => SignalInfo(name, desc, ArrowDirection.PointRight, null)
-      case outRe(name, desc) => SignalInfo(name, desc, ArrowDirection.PointLeft, null)
-      case inoutRe(name, desc) => SignalInfo(name, desc, ArrowDirection.PointBoth, null)
+      case inRe(name, desc) => SignalInfo(name, desc, ArrowDirection.PointRight, arrowStyle)
+      case outRe(name, desc) => SignalInfo(name, desc, ArrowDirection.PointLeft, arrowStyle)
+      case inoutRe(name, desc) => SignalInfo(name, desc, ArrowDirection.PointBoth, arrowStyle)
     })
-
     ComponentInfo(componentName, Array(groupsLeft), Array())
   }
 }
